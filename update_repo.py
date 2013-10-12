78af8d20a106950298330e79e8a0c4591019bb8f
Frueh Rentner <fruehrentner@gmail.com>
2013-10-12 12:07:36 +0200

    Fixed update_repo.xml. Now we clear repo in the first step

--- update_repo.py
+++ update_repo.py
@@ -5,14 +5,14 @@ SRC_DIR=src
 REPO_DIR=repo
 REPO_NAME=repository.tuffnerdstuff
 
+# Clear repo dir
+rm -r $REPO_DIR/*
+
 # Create addons.xml
 cd $SRC_DIR
 $PY_BIN addons_xml_generator.py
 mv addons.xml ../$REPO_DIR
 mv addons.xml.md5 ../$REPO_DIR
-
-# Clear repo dir
-rm -r ../$REPO_DIR/*
 
 # Create ZIP files
 for d in $(find . -mindepth 1 -maxdepth 1 -type d);
