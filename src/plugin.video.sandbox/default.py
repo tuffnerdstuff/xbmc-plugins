db888f509f8a377804aa198333145d3cada0cb65
Frueh Rentner <fruehrentner@gmail.com>
2013-10-13 00:33:30 +0200

    Fixed sandbox plugin

--- src/plugin.video.sandbox/default.py
+++ src/plugin.video.sandbox/default.py
@@ -5,7 +5,7 @@ name = 'Zero Punctuation'
 url = 'http://video.escapistmagazine.com/links/766d9ea2d7d02d7e2dec3a7c47146f9d/mp4/escapist/zero-punctuation/c18de1cf26b7b6078077180f3941a116.mp4'
 
 def addDirectoryItem():
-    li = xbmcgui.ListItem(name,iconImage="DefaultFolder.png", thumbnailImage=pic)
+    li = xbmcgui.ListItem(name,iconImage="DefaultFolder.png", thumbnailImage="")
     return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=li, isFolder=True)
 
 def addPlayableItem():
